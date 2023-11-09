fbb3ca831d707e4293de9f6a4fd706f6d2cd66dd
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-11-09 16:07:38 -0300

    Initial setup. 1. Created Todo model and TodoList Notifier that holds the data of the todos present in the app; 2. Added the NotifierProvider in the global scope 3. Add new todo in floating button to experimente provider read and watch behaviours.

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_todo_app/todo.dart';
+
+final todoListProvider = NotifierProvider<TodoList, List<Todo>>(TodoList.new);
+
+void main() {
+  runApp(const ProviderScope(child: MainApp()));
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Consumer(
+          builder: (context, ref, child) {
+            final todos = ref.watch(todoListProvider);
+            return ListView(
+              children: [
+                for (final todo in todos)
+                  Container(
+                    alignment: Alignment.center,
+                    color: Color.fromRGBO(Random().nextInt(256),
+                        Random().nextInt(256), Random().nextInt(256), 1),
+                    height: 250,
+                    child: Text(todo.description),
+                  )
+              ],
+            );
+          },
+        ),
+        floatingActionButton: Consumer(builder: (context, ref, _) {
+          return FloatingActionButton(
+            key: const Key('increment_floatingActionButton'),
+            // The read method is a utility to read a provider without listening to it
+            onPressed: () =>
+                ref.read(todoListProvider.notifier).add('Learn Riverpod'),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          );
+        }),
+      ),
+    );
+  }
+}
